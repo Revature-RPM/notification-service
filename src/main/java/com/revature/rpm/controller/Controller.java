8a27b8e5baa2345affde31e7c8341bfecb1a3040
Emad Davis <emaddavis@gmail.com>
2019-12-02 13:43:05 -0500

    made services, models, and started writing tests

---
+++ src/main/java/com/revature/rpm/controller/Controller.java
@@ -0,0 +1,28 @@
+package com.revature.rpm.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.rpm.dto.ReadDTO;
+import com.revature.rpm.services.NotificationService;
+
+@RestController
+@RequestMapping("")
+public class Controller {
+	private NotificationService notificationService;
+	
+	@Autowired
+	public Controller(NotificationService notificationService) {
+		super();
+		this.notificationService = notificationService;
+	}
+
+	@PatchMapping("/")
+	
+	public void updateRead(@RequestBody ReadDTO readDTO) {
+		notificationService.updateUnreadToRead(readDTO);
+	}
+}
