d71692255da2fc91a465dd8de4945eee5d72ab9f
BubbaTing <ronbubbating@hotmail.com>
2019-12-02 09:55:46 -0500

    initial testing

---
+++ src/main/java/testing/models/Rectangle.java
@@ -0,0 +1,41 @@
+package testing.models;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value = "Rectangle")
+public class Rectangle extends Shape {
+
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "width=" + width +
+                ", length=" + length +
+                '}';
+    }
+}
