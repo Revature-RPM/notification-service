3dfb240648c014578410240aba77f0e6cbb4783f
ChrisTroll <ctroll333@gmail.com>
2019-12-04 14:21:29 -0500

    commit before pulling from rev-dev

--- src/main/java/com/revature/rpm/services/ListenerService.java
+++ src/main/java/com/revature/rpm/services/ListenerService.java
@@ -1,5 +1,31 @@
 package com.revature.rpm.services;
 
-public interface ListenerService {
+import javax.jms.JMSException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.amazon.sqs.javamessaging.ProviderConfiguration;
+import com.amazon.sqs.javamessaging.SQSConnection;
+import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+
+@Service
+public class ListenerService {
+	public SQSConnectionFactory connectionFactory = new SQSConnectionFactory(
+	        new ProviderConfiguration(),
+	        AmazonSQSClientBuilder.defaultClient()
+	        );
+	// Create the connection.
+	public SQSConnection connection = null;
+	
+	@Autowired
+	public ListenerService() {
+		try {
+			connection = connectionFactory.createConnection();
+		} catch (JMSException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
