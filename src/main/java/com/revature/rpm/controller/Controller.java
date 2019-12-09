d719673906771407bec301afca137ddd3ad35400
ChrisTroll <ctroll333@gmail.com>
2019-12-09 13:25:24 -0500

    adds ability to read userId from JWTs

--- src/main/java/com/revature/rpm/controller/Controller.java
+++ src/main/java/com/revature/rpm/controller/Controller.java
@@ -8,11 +8,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.rpm.dto.ReadDTO;
 import com.revature.rpm.entities.Comment;
+import com.revature.rpm.services.JWTService;
 import com.revature.rpm.services.NotificationService;
 
 @RestController
@@ -20,20 +22,25 @@ import com.revature.rpm.services.NotificationService;
 public class Controller {
 	
 	NotificationService notificationService;
+	JWTService jwtserv;
 	
 	@Autowired
-	public Controller(NotificationService notificationService) {
+	public Controller(NotificationService notificationService, JWTService jwtserv) {
 		super();
 		this.notificationService = notificationService;
+		this.jwtserv = jwtserv;
 	}
 	
 	@GetMapping("/")
-	public List<Comment> getAllNewNotifications() {
+	public List<Comment> getAllNewNotifications(@RequestHeader("Authorization") String jws) {
+		int userid = jwtserv.extractUserIdFromJWT(jws);
+		System.out.println(userid);
 		return notificationService.getAllNewNotifications();
 	}
 	
 	@GetMapping("/history")
-	public Page<Comment> getNotificationsByPage(Pageable page) {
+	public Page<Comment> getNotificationsByPage(@RequestHeader("Authorization") String jws, Pageable page) {
+		int userid = jwtserv.extractUserIdFromJWT(jws);
 		return notificationService.getNotificationsByPage(page);
 	}
 
