62a47409880135f9058842688b5d7ed58cb1df56
James Meadows <jamesmeadows87@gmail.com>
2019-12-10 16:36:00 -0500

    commiting to commit

--- src/main/java/com/revature/rpm/Application.java
+++ src/main/java/com/revature/rpm/Application.java
@@ -2,15 +2,7 @@ package com.revature.rpm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/**
- * 
- * @author James Meadows
- * @author Stefano Georges
- * @author Chong Ting
- * @author Christopher Troll
- * @author Emad Davis
- *
- */
+
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) throws Exception {
